336abdd3e3e705e59f2ff325ed6240b8f680c821
Kevin Leung <kevinresol@gmail.com>
2020-01-04 19:43:05 +0800

    Add props

--- src/react/composition_input/CompositionInput.hx
+++ src/react/composition_input/CompositionInput.hx
@@ -1,10 +1,14 @@
 package react.composition_input;
 
 import react.ReactComponent;
+import js.html.Event;
+import js.html.InputElement;
 
 @:jsRequire('react-composition-input', 'default')
 extern class CompositionInput extends ReactComponentOfProps<{
 	?onChange:String->Void,
+	?onInputChange:Event->Void,
+	?inputRef:InputElement->Void,
 	?id:String,
 	?value:String,
 	?placeholder:String,
